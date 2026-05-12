f4cc8d90e25282ec9b5f8e671dccf351ba6040b4
Cristofer Figueroa <cristo.figueroam@duocuc.cl>
2026-05-11 21:43:35 -0400

    Add files via upload

---
+++ MS-Financiamiento/MS-Financiamiento/src/main/java/com/AutoMaster/MS_Financiamiento/MsFinanciamientoApplication.java
@@ -0,0 +1,13 @@
+package com.AutoMaster.MS_Financiamiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFinanciamientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFinanciamientoApplication.class, args);
+	}
+
+}
---
+++ MS-Financiamiento/MS-Financiamiento/src/test/java/com/AutoMaster/MS_Financiamiento/MsFinanciamientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AutoMaster.MS_Financiamiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFinanciamientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
